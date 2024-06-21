41f20e475c107587c4dc64c1f5e888ef5907bea1
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2024-06-21 16:55:42 +0300

    init commit

---
+++ ToDo App/App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo App
+//
+//  Created by Мирсаит Сабирзянов on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDo App/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ToDo App
+//
+//  Created by Мирсаит Сабирзянов on 14.06.2024.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+    var body: some View {
+        Image(systemName: "face.smiling")
+            .font(.system(size: 100))
+            .foregroundStyle(.yellow)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
